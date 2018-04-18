efab0248e7d899f1a64bde792f0b5c14e5a5eb27
Kanishk Sharma <kanishksharma3012@gmail.com>
2018-04-18 12:39:45 +0530

    Basic database reducer setup

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+class App extends Component {
+
+    render() {
+        return (
+            <Provider>
+                <View>
+                    <Text>
+                        Hi! Welcome to Realm!
+                    </Text>
+                </View>
+            </Provider>
+        );
+    }
+}
+
+export default App;
